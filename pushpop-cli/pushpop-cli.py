ea7336f7d00623a4027c5eb7020a414db69a4a91
Daniel Hall <daniel.hall@rea-group.com>
2012-01-17 19:53:48 +1100

    Added initial files for everything

---
+++ pushpop-cli/pushpop-cli.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+print "Talk to the core API here. No not the Australian Property Institute..."
---
+++ pushpop-client/pushpop-client.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+print "Run a check and send it to The Core (not the crappy movie with Hilary Swank in it)"
+
