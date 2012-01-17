cc8b29362927544de6bf3eb42b78dc6ec49c98e8
Daniel Hall <daniel.hall@rea-group.com>
2012-01-17 20:18:52 +1100

    Enable the admin site

--- pushpopweb/urls.py
+++ pushpopweb/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# Admin functionality
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pushpopweb.views.home', name='home'),
     # url(r'^pushpopweb/', include('pushpopweb.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Admin Page
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
